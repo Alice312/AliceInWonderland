a433e6a670ffb26ff2c6f3326c109e99e9a608a2
Alice Ivey <janara7@Alices-Air.lan>
2022-04-19 10:34:00 -1000

    This is new class added

--- src/com/java/class16/Mulof1to5.java
+++ src/com/java/class16/Mulof1to5.java
@@ -1,2 +1,11 @@
-package com.java.class16;public class Mulof1to5 {
-}
+package com.java.class16;
+
+public class Mulof1to5 {
+    public static void main(String[] args) {
+        int sum=1;
+        for(int i=1; i<=5;i++){
+            sum=sum*i;}
+            System.out.println(sum);
+        }
+    }
+
