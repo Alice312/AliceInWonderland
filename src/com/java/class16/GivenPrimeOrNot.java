685ebb552846adccf412d53c3d6d23c51311a077
Alice Ivey <janara7@Alices-Air.lan>
2022-04-19 10:33:06 -1000

    This is new added class

--- src/com/java/class16/GivenPrimeOrNot.java
+++ src/com/java/class16/GivenPrimeOrNot.java
@@ -1,2 +1,16 @@
-package com.java.class16;public class GivenPrimeOrNot {
-}
+package com.java.class16;
+import java.util.Scanner;
+public class GivenPrimeOrNot {
+    public static void main(String[] args) {
+        //write a program to check given number is prime or not
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Write any number:");
+        int num= sc.nextInt();
+        int count=0;
+    for(int i=1; i<=num; i++) {
+        if (num % i == 0) {
+            count++;}}
+        if(count==2){
+            System.out.println(num+ " is a Prime Number!");
+        }else{
+            System.out.println(num+" is not a Prime Number!");}}}
