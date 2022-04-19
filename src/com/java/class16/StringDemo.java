031d6b8ba735d58d8c8b87ab9fe38f6f8a8ba1a0
Alice Ivey <janara7@Alices-Air.lan>
2022-04-19 10:36:42 -1000

    This is new added class from yesterdays practice

--- src/com/java/class16/StringDemo.java
+++ src/com/java/class16/StringDemo.java
@@ -1,2 +1,20 @@
-package com.java.class16;public class StringDemo {
+package com.java.class16;
+
+import java.util.Scanner;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str="Hello";
+        StringBuffer sBuffer =new StringBuffer("Hello");
+        sBuffer.reverse();
+        //string buffer uses the buffer mechanism to protect data
+        //from being access and update simultaneouly, soi ts a thread-safe
+
+        StringBuilder sBuilder=new StringBuilder("Hello");
+        sBuilder.reverse();
+        //doesn't have buffer mechanism so its faster than StringBuffer
+
+
+    }
 }
--- src/com/java/class16/StringPractice1.java
+++ src/com/java/class16/StringPractice1.java
@@ -1,2 +1,17 @@
-package com.java.class16;public class StringPractice1 {
+package com.java.class16;
+//write a program to take one statement from user an find out number of words
+//we have inside the statement
+
+//Ex: Hello this is my first java string program
+//output: 8
+
+import java.util.Scanner;
+
+public class StringPractice1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String [] parts=str.split(" ");
+        System.out.println(parts.length);
+    }
 }
