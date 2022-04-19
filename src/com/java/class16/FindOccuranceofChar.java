e580b0a20a333936ed808304fc42dedc0dbc1fdd
Alice Ivey <janara7@Alices-Air.lan>
2022-04-19 10:16:13 -1000

    All of the class 16 are new added package and classes

--- src/com/java/class16/FindOccuranceofChar.java
+++ src/com/java/class16/FindOccuranceofChar.java
@@ -1,2 +1,20 @@
-package com.java.class16;public class FindOccuranceofChar {
+package com.java.class16;
+//write a program to take one String from user and print
+// occurrence of given char in String
+//input: this is my java program!
+//output-3
+
+public class FindOccuranceofChar {
+    public static void main(String[] args) {
+        String str="This is my java program!";
+        char c='a';
+        int count=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)==c){
+                count++;
+            }
+
+        }
+        System.out.println(count);
+    }
 }
