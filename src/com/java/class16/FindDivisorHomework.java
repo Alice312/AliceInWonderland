da0eeb632318f904e58fd500608ee16165a98d8a
Alice Ivey <janara7@Alices-Air.lan>
2022-04-21 16:37:47 -1000

    This is new package/class added for java class

---
+++ src/com/java/class16/FindDivisorHomework.java
@@ -0,0 +1,8 @@
+package com.java.class16;
+public class FindDivisorHomework {
+    public static void main(String[] args) {
+        System.out.println("Below is the numbers divisible by 6:");
+        int num=6;
+        for(int i=1; i<=num; i++){
+            if(num%i==0){
+                System.out.println(i);}}}}
